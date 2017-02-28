9e9fe4a727a73d98bcad9c12592cb95870a0c407
Raditya <arya.raditya26@gmail.com>
2017-02-28 16:34:10 +0700

    Done release 4 & 5

--- src/App.js
+++ src/App.js
@@ -2,24 +2,7 @@ import React, { Component } from 'react';
 import { List } from './List';
 import { Form } from './Form';
 
-const data = [
-  {
-    title : 'React',
-    url : 'https://facebook.github.io/react/',
-    author : 'Jordan Walke',
-    num_comments : 3,
-    points : 4,
-    objectID : 0,
-  },
-  {
-    title : 'Redux',
-    url : 'https://github.com/reactjs/redux',
-    author : 'Dan Abramov, Andrew Clark',
-    num_comments : 2,
-    points : 5,
-    objectID : 1,
-  },
-]
+const data = []
 
 class App extends Component {
   constructor(){
@@ -28,28 +11,25 @@ class App extends Component {
       data,
       search : ''
     }
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   searchTitle (e) {
-    this.setState({
-      search : e.target.value
-    })
+
+    fetch(`http://hn.algolia.com/api/v1/search?query=${e.target.value}`)
+        .then(res => res.json())
+        .then(data => this.setState ({data: data.hits}))
   }
 
   render(){
 
-    // if(search > 0){
-    //   var libraries = data.filter( (l) => {
-    //     return l.title.toLowerCase().match(search)
-    //   })
-    // }
-
     return(
       <div>
         <Form searchTitle={this.searchTitle.bind(this)} search={this.state.handleChange} />
-        <List data={this.state.data.filter( result => result.title.toLowerCase().match(this.state.search.toLowerCase()))} />
+        <List data={this.state.data.filter( result => {
+            const title = result.title === null ? '' : result.title;
+            if (title !== '') return title.toLowerCase().match(this.state.search.toLowerCase())
+          }
+        )} />
       </div>
     )
   }
