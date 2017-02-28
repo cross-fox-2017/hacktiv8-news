c18c986f643a3cc4fd732c25a21ca2df60460de0
timocodex <timothy.og.siahaan@gmail.com>
2017-02-28 13:56:26 +0700

    done release 4

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
-import {NewsList} from './components';
+import {NewsList,NewsForm} from './components';
 
-const data = [
-  {
-    title: 'React',
-    url:'https://facebook.github.io/react/',
-    author:'jordan wilke',
-    num_comments:3,
-    points:4,
-    objectID:0
-  },
-  {
-    title: 'Redux',
-    url:'https://github.com/reactjs/redux',
-    author:'Dan Abramov,Andrew Clark',
-    num_comments:2,
-    points:5,
-    objectID:1
-  }
-]
+
 class App extends Component {
   constructor(){
     super()
     this.state={
-      link: data
+      link: [
+        {
+          title: 'React',
+          url:'https://facebook.github.io/react/',
+          author:'jordan wilke',
+          num_comments:3,
+          points:4,
+          objectID:0
+        },
+        {
+          title: 'Redux',
+          url:'https://github.com/reactjs/redux',
+          author:'Dan Abramov,Andrew Clark',
+          num_comments:2,
+          points:5,
+          objectID:1
+        }
+      ],
+      searchLink:''
     }
+  }
+
+  handleChange(event){
+    this.setState({
+      searchLink: event.target.value
+    })
   }
   render() {
     console.log(this.state.link);
     return (
-      <NewsList links={this.state.link}/>
+      <div>
+        <NewsForm handleChange={this.handleChange.bind(this)} value={this.state.searchLink}/>
+        <NewsList links={this.state.link.filter((x)=>{return x.title.toLowerCase().match(this.state.searchLink.toLowerCase())})}/>
+      </div>
     );
   }
 }
