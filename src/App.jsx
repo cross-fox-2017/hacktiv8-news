0f55c4ab8bf4517ae42d5021973f69ddbfbfad4c
Syanmil <syanmil@yahoo.com>
2017-02-28 11:33:44 +0700

    moving from global variable to states

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const data = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    objectID: 1
-  }
-]
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [
+        {
+          title: 'React',
+          url: 'https://facebook.github.io/react/',
+          author: 'Jordan Walke',
+          objectID: 0
+        },
+        {
+          title: 'Redux',
+          url: 'https://github.com/reactjs/redux',
+          author: 'Dan Abramov, Andrew Clark',
+          objectID: 1
+        }
+      ]
+    }
+  }
   render() {
     return (
       <ul>
-        {data.map((item)=>{
+        {this.state.data.map((item)=>{
           return (
               <li>
                 <a href={item.url} target="_blank">{item.title}</a>
