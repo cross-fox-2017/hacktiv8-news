c2af3598a5f991d730012f89d7029ea044c701b9
Muhammad Iqbal <iqbal.0809@gmail.com>
2017-02-28 15:19:56 +0700

    done release 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import {List} from './components/List.js'
+import {List} from './components/List'
+import {Form} from './components/Form'
+
+
 
 const data = [
   {
@@ -15,11 +18,29 @@ const data = [
 class App extends Component {
   constructor() {
     super()
-    this.state = {data}
+    this.state = {
+      data,
+      keyword: ''
+    }
   }
+
+  searchTitle (event) {
+    this.setState({
+      keyword: event.target.value
+    })
+    fetch(`http://hn.algolia.com/api/v1/search?query=${event.target.value}`)
+    .then(res => res.json())
+    .then(data => this.setState ({data: data.hits}))
+  }
+
+
+
   render() {
     return (
-      <List data={this.state.data}/>
+      <div>
+        <List data={this.state.data.filter(results=> (results.title === null ? '' : results.title).match(new RegExp(this.state.keyword,'i')))}/>
+        <Form searchTitle={this.searchTitle.bind(this)} keyword={this.state.keyword}/>
+      </div>
     );
   }
 }
