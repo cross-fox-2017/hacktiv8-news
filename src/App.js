8f456b8af35206baf1c1c5bf48a6d0bb85330b0f
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-28 11:17:25 +0700

    Done release 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
+const data = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.io/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <ul>
+        {data.map((item, index) => {
+           return (
+             <li key={index}>
+               <a href={item.url}>
+                 {item.title}
+               </a>
+             </li>
+           )
+         })}
+      </ul>
+    )
   }
 }
 
-export default App;
+export default App
